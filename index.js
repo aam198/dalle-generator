656017465b115f1bcbb5513e4e407401fc6067cd
aam198 <aam198@psu.edu>
2022-12-17 11:51:39 -0500

    Frontend initial build

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 8000;
+const path = require('path');
 
 const app = express();
 
 // Enabe body parser, to accept body data
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Connect to openaiRoutes Route
 app.use('/openai', require('./routes/openaiRoutes'));
